6cf50688730fed655d06448803b43068c7885496
davidfayanju1 <davidfayanju01@gmail.com>
2024-12-25 18:22:45 +0100

    styling changes

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="page-container min-h-[10rem] w-full bg-green-950">
+    <footer className="page-container min-h-[20rem] md:px-[4rem] px-[1rem] py-[3rem] w-full bg-green-50">
       <div className="title">
-        <img src="/images/Logo.jpg" alt="" />
+        <img src="/icons/Logo.svg" alt="" />
       </div>
     </footer>
   );
--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
   ];
 
   return (
-    <div className="nav-container z-50 bg-white/50 backdrop-blur-lg fixed top-0 w-full h-[4rem] flex items-center justify-between md:px-[4rem] px-[1rem] shadow-md">
+    <div className="nav-container z-50 bg-white backdrop-blur-lg fixed top-0 w-full h-[4rem] flex items-center justify-between md:px-[4rem] px-[1rem] shadow-md">
       <div className="page_title">
         <Link to={"/"} className="flex items-center gap-1">
           <h1 className="serif-regular text-green-950 font-bold md:text-[1.3rem] text-[1.1rem]">
